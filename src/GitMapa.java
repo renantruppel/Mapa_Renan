ae3cc5a47d91b18c4858a612ecc7e9c5d87cdbb2
Renan Truppel Ayoub <r_truppel20@hotmail.com>
2024-06-29 08:46:55 -0300

    Fazendo mudança na branch atividade mapa

--- src/GitMapa.java
+++ src/GitMapa.java
@@ -1,5 +1,5 @@
 public class GitMapa {
     public static void main(String[] args) throws Exception {
-        System.out.println(" Utilizando o Git e Github!");
+        System.out.println(" Utilizando o Git e Github! Change from other branch");
     }
 }
